77c1318f5a5f9c7c478da2aa051a904a20697b2e
Basanagouda-2006 <basanagoudad82@gmail.com>
2026-07-26 15:39:46 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import hotelDetailsViewSet
+
+router = DefaultRouter()
+
+router.register(r'hotelDetails', hotelDetailsViewSet)
+
+urlpatterns = router.urls
